3b1cd34d3a25c9f31a2eec09de011c553d761e75
GaranmMoon <garan.moon@eghishc.co.kr>
2024-08-18 02:17:12 +0900

    [ConfigButtonSet] 빈 그림 export 못하는 것으로 변경

--- src/component/organism/ConfigButtonSet.tsx
+++ src/component/organism/ConfigButtonSet.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { ButtonGroup } from 'component/molecule';
 import { useExport } from 'hook/useExport';
 import { useReset } from 'hook/useReset';
@@ -6,16 +8,24 @@ import { useColorMapStore } from 'store/ColorMapStore';
 function ConfigButtonSet() {
   const { save, share } = useExport();
   const { confirmReset } = useReset();
-  const { history } = useColorMapStore((state) => state);
-  const disabled = history.length <= 1;
+  const { colorMap, history, historyIndex } = useColorMapStore((state) => state);
+  const [disableExport, setDisableExport] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (JSON.stringify(colorMap) === '{}') {
+      setDisableExport(true);
+      return;
+    }
+    setDisableExport(false);
+  }, [historyIndex]);
 
   return (
     <ButtonGroup
       direction="column"
       buttons={[
-        { title: 'SAVE', disabled, onClick: () => save() },
-        { title: 'SHARE', disabled, onClick: () => share() },
-        { title: 'RESET', disabled, onClick: () => confirmReset() },
+        { title: 'SAVE', disabled: disableExport, onClick: () => save() },
+        { title: 'SHARE', disabled: disableExport, onClick: () => share() },
+        { title: 'RESET', disabled: history.length <= 1, onClick: () => confirmReset() },
       ]}
     />
   );
