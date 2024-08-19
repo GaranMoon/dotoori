7b62c38d4e2eafd480f96077bc2928d1680192b8
GaranmMoon <garan.moon@eghishc.co.kr>
2024-08-20 02:53:59 +0900

    [GridEditor] 모바일 연속 그리기 기능 추가

--- src/component/organism/GridEditor.tsx
+++ src/component/organism/GridEditor.tsx
@@ -1,17 +1,53 @@
+import { useEffect, useState } from 'react';
+
+import { getColorMapKey } from 'util/common';
+
 import { Grid, GridType } from 'component/atom';
 import { useColorMapStore } from 'store/ColorMapStore';
 import { useToolStore } from 'store/ToolStore';
 import { Tool } from 'type/common';
+
+type Pointer = { key: string; isStart?: boolean };
 
 interface Props {
   mode: GridType;
 }
 
 function GridEditor({ mode }: Props) {
+  const [pointer, setPointer] = useState<Pointer>();
   const { tool, picker, isOn, setIsOn } = useToolStore((state) => state);
   const { colorMap, setColorMap } = useColorMapStore((state) => state);
 
-  const handleDraw = (key: string, isStart?: boolean) => {
+  useEffect(() => {
+    if (pointer) handleDraw(pointer);
+  }, [pointer]);
+
+  useEffect(() => {
+    const table = document.getElementById('edit')?.querySelector('table');
+    if (!table) return;
+    const handleTouch = (event: TouchEvent, isStart?: boolean) => {
+      const touch = event.touches[0];
+      if (!touch) return;
+      const { clientX, clientY } = touch;
+      if (!clientX || !clientY) return;
+      const elementTouched = document.elementFromPoint(touch.clientX, touch.clientY);
+      if (elementTouched?.tagName !== 'TD') return;
+      const td = elementTouched as HTMLTableCellElement;
+      const tr = td.parentElement as HTMLTableRowElement;
+      const yIndex = Array.from(tr.parentElement!.children).indexOf(tr);
+      const xIndex = Array.from(tr.children).indexOf(td);
+      const key = getColorMapKey(xIndex, yIndex);
+      setPointer({ key, isStart });
+    };
+    table.addEventListener('touchstart', (e) => handleTouch(e, true));
+    table.addEventListener('touchmove', (e) => handleTouch(e));
+    return () => {
+      table.removeEventListener('touchstart', (e) => handleTouch(e, true));
+      table.removeEventListener('touchmove', (e) => handleTouch(e));
+    };
+  }, []);
+
+  const handleDraw = ({ key, isStart }: Pointer) => {
     if (!tool || (!isStart && !isOn)) return;
     if (!isOn) setIsOn(true);
     if (tool === Tool.ERASER) {
@@ -27,10 +63,8 @@ function GridEditor({ mode }: Props) {
     <Grid
       mode={mode}
       tdProps={(key) => ({
-        onMouseDown: () => handleDraw(key, true),
-        onMouseMove: () => handleDraw(key),
-        onTouchStart: () => handleDraw(key, true),
-        onTouchMove: () => handleDraw(key),
+        onMouseDown: () => setPointer({ key, isStart: true }),
+        onMouseMove: () => setPointer({ key }),
       })}
     />
   );
