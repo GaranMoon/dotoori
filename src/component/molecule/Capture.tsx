bd9bf40ac7fcb1c1a660eae90f7d26ca2bab4eb9
GaranmMoon <garan.moon@eghishc.co.kr>
2024-08-22 19:07:46 +0900

    [Watermark] 컴포넌트 구현

--- src/component/molecule/Capture.tsx
+++ src/component/molecule/Capture.tsx
@@ -2,15 +2,17 @@ import { useMemo } from 'react';
 
 import { getColorMapKey } from 'util/common';
 
+import chroma from 'chroma-js';
+import { Watermark } from 'component/atom';
 import { useColorMapStore } from 'store/ColorMapStore';
 import { useSettingStore } from 'store/SettingStore';
-import { SAVE_MAX_SIZE } from 'type/common';
+import { BackgroundColor, SAVE_MAX_SIZE } from 'type/common';
 
 import styles from './Capture.module.scss';
 
 function Capture() {
   const { colorMap } = useColorMapStore((state) => state);
-  const { numOfBoxs } = useSettingStore((state) => state);
+  const { numOfBoxs, backgroundColor } = useSettingStore((state) => state);
 
   const { boxStyle, containerStyle } = useMemo(() => {
     const boxSize = Math.floor(SAVE_MAX_SIZE / numOfBoxs);
@@ -20,6 +22,23 @@ function Capture() {
       containerStyle: { width: containerSize, height: containerSize },
     };
   }, [numOfBoxs]);
+
+  const getInvertedColor = () => {
+    const targetBoxs = Math.ceil(numOfBoxs / 5) + 1;
+    const colors: string[] = [];
+    for (let i = numOfBoxs; i > numOfBoxs - targetBoxs; i--) {
+      const key = getColorMapKey(i - 1, numOfBoxs - 1);
+      const color = colorMap[key] || (backgroundColor === BackgroundColor.BLACK ? '#000000' : '#ffffff');
+      colors.push(color);
+    }
+    const averageColor = chroma.average(colors).hex();
+    const invertedColor = chroma(
+      255 - chroma(averageColor).get('rgb.r'),
+      255 - chroma(averageColor).get('rgb.g'),
+      255 - chroma(averageColor).get('rgb.b'),
+    ).hex();
+    return invertedColor;
+  };
 
   return (
     <div id="capture" className={styles.container} style={containerStyle}>
@@ -37,6 +56,9 @@ function Capture() {
           })}
         </div>
       ))}
+      <div className={styles.watermark}>
+        <Watermark color={getInvertedColor()} />
+      </div>
     </div>
   );
 }
