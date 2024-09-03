49c5e85af5d6d4948df5a325f78b0bcbd3414efd
GaranmMoon <garan.moon@eghishc.co.kr>
2024-09-03 22:31:27 +0900

    [Capture] 배경색 관련 버그 수정

--- src/component/molecule/Capture.tsx
+++ src/component/molecule/Capture.tsx
@@ -14,15 +14,21 @@ function Capture() {
   const backgroundColor = useSettingStore((state) => state.backgroundColor);
   const boxSize = Math.floor(SAVE_MAX_SIZE / numOfBoxs);
   const containerSize = boxSize * numOfBoxs;
+  const emptyColor =
+    backgroundColor === BackgroundColor.WHITE
+      ? 'white'
+      : backgroundColor === BackgroundColor.BLACK
+        ? 'black'
+        : undefined;
+  const containerStyle = { width: containerSize, height: containerSize, backgroundColor: emptyColor };
   const boxStyle = { width: boxSize, height: boxSize };
-  const containerStyle = { width: containerSize, height: containerSize };
 
   const getInvertedColor = () => {
     const targetBoxs = Math.ceil(numOfBoxs / 5) + 1;
     const colors: string[] = [];
     for (let i = numOfBoxs; i > numOfBoxs - targetBoxs; i--) {
       const key = getColorMapKey(i - 1, numOfBoxs - 1);
-      const color = colorMap[key] || (backgroundColor === BackgroundColor.WHITE ? '#ffffff' : '#000000');
+      const color = colorMap[key] || emptyColor || 'black';
       colors.push(color);
     }
     const averageColor = chroma.average(colors).hex();
